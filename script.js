454b5d227d7956d1cf45fecd4298c2c2ec735562
domoyna <douomouo@gmail.com>
2024-01-26 06:22:37 +0300

    inject forecast HRML from JavaScript

--- script.js
+++ script.js
@@ -53,8 +53,37 @@ function handleSearchSubmit(event) {
   let searchInput = document.querySelector("#search-form-input");
   searchCity(searchInput.value);
 }
+function displayForecast() {
+  let forecast = document.querySelector("#forecast");
+  let days = [
+    "Tue",
+    "Wed",
+    "Thu",
+    "Fri",
+    "Sat"
+  ]
+  let forecastHtml = "";
+  days.forEach(function (day) {
+    forecastHtml =
+    forecastHtml +
+    `
+    <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon"></div>
+        <div class="weather-forecast-temperatures">
+        <div class-"weather-forecast-temperature-max"><strong>18°<strong></div>
+        <div class="weather-forecast-temperature-min">15°</div>
+        </div>
+        </div>
+        `;
+  });
+  let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML = forecastHtml;
+}
+
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Antananarivo");
+displayForecast();
