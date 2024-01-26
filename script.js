4c826156aa15aff7a7a2e512f909c598227903c7
domoyna <douomouo@gmail.com>
2024-01-26 15:29:08 +0300

    45 line updated

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function formatDate(date) {
 }
 function searchCity(city) {
   let apiKey = "fbef01f4et1b02o0d25c27210a43ef3f";
-  let apiUrl = ``https://api.shecodes.io/weather/v1/current?query=${encodeURIComponent(
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${encodeURIComponent(
     city
   )}&key=${apiKey}&units=metric`;
   console.log(apiUrl);
@@ -57,7 +57,7 @@ function handleSearchSubmit(event) {
 
 function getForecast(city) {
   let apiKey="fbef01f4et1b02o0d25c27210a43ef3f";
-  let apiUrl ="https://api.shecodes.io/weather/v1/forecast?query=${encodeURIComponent(
+  apiUrl =`https://api.shecodes.io/weather/v1/forecast?query=${encodeURIComponent(
     city
   )}&key=${apiKey}&units=metric`;
   axios(apiUrl).then(displayForecast);
