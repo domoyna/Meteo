3b750023965f7d3b00a56b8a0cf30a387cc0cdf7
domoyna <douomouo@gmail.com>
2024-01-26 05:29:38 +0300

    flex direction added

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function formatDate(date) {
 }
 function searchCity(city) {
   let apiKey = "fbef01f4et1b02o0d25c27210a43ef3f";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${encodeURIComponent(
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query==${encodeURIComponent(
     city
   )}&key=${apiKey}&units=metric`;
   console.log(apiUrl);
